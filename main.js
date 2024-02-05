63a047e6057c3e2c6b5c555bd4919dd8f0614bfa
RyanTanMing <125744706+RyanTanMing@users.noreply.github.com>
2024-02-05 17:26:45 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+song=""
+function setup() {
+    canvas=createCanvas(500,500)
+    canvas.center()
+    video=createCapture(VIDEO)
+    video.hide()
+}
+function draw() {
+    image(video, 0,0,500,500)
+}
+function preload() {
+    song=loadSound("music.mp3")
+}
+function play() {
+    song.play()
+}
+function stop() {
+    song.stop()
+}
