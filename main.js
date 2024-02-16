844260cbaa675b0b58b58ff1818f122f4fd7d53d
RyanTanMing <125744706+RyanTanMing@users.noreply.github.com>
2024-02-16 17:00:46 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ function setup() {
     canvas.center()
     video=createCapture(VIDEO)
     video.hide()
+    pn=ml5.poseNet(video,model_loaded)
+    pn.on("pose",got_poses)
 }
 function draw() {
     image(video, 0,0,500,500)
@@ -16,4 +18,27 @@ function play() {
 }
 function stop() {
     song.stop()
+}
+leftwristX=0
+leftwristY=0
+rightwristX=0
+rightwristY=0
+
+
+    pn=ml5.poseNet(video,model_loaded)
+    pn.on("pose",got_poses)
+
+function model_loaded() {
+    console.log("poseNet has been loaded")
+}
+function got_poses(results) {
+    if (results.length>0) {
+        console.log(results)
+        leftwristX=results[0].pose.leftWrist.x
+        leftwristY=results[0].pose.leftWrist.y
+        rightwristX=results[0].pose.rightWrist.x
+        rightwristY=results[0].pose.rightWrist.y
+        console.log(leftwristX,leftwristY)
+        console.log(rightwristX,rightwristY)
+    }
 }
