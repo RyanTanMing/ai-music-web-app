2184ed76d9c25dc1245f633c59fc2c4b9cec6128
RyanTanMing <125744706+RyanTanMing@users.noreply.github.com>
2024-02-19 17:18:17 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
 song=""
+song2=""
+scoreleftwrist=0
+scorerightwrist=0
 function setup() {
     canvas=createCanvas(500,500)
     canvas.center()
@@ -9,12 +12,27 @@ function setup() {
 }
 function draw() {
     image(video, 0,0,500,500)
+    fill("red")
+    stroke("red")
+    if (scoreleftwrist>0.1) {
+    circle(leftwristX,leftwristY,20)
+    n=Number(leftwristY)
+    n1=floor(n)
+    play(song)
+    }
+    if (scorerightwrist>0.1) {
+        circle(rightwristX,rightwristY,20)
+        n=Number(rightwristY)
+        n1=floor(n)
+        play(song2)
+        }
 }
 function preload() {
     song=loadSound("music.mp3")
+    song2=loadSound("1_In1_In_His_Hands (1).mp3")
 }
-function play() {
-    song.play()
+function play(songname) {
+    songname.play()
 }
 function stop() {
     song.stop()
@@ -36,8 +54,10 @@ function got_poses(results) {
         console.log(results)
         leftwristX=results[0].pose.leftWrist.x
         leftwristY=results[0].pose.leftWrist.y
+        scoreleftwrist=results[0].pose.keypoints[9].score
         rightwristX=results[0].pose.rightWrist.x
         rightwristY=results[0].pose.rightWrist.y
+        scorerightwrist=results[0].pose.keypoints[17].score
         console.log(leftwristX,leftwristY)
         console.log(rightwristX,rightwristY)
     }
